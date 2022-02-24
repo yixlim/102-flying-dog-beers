1817a3bc7ad067c4e562d9bf9c94708ddaf6d73b
Yi Xiang Lim <yixiang_lim@intuit.com>
2022-02-24 02:41:22 +0000

    made changes

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+# from dash import 
+# from dash import 
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+beers=['Chocolate Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
+color1='brown'
+color2='blue'
 mytitle='Beer Comparison'
 tabtitle='beer!'
 myheading='Flying Dog Beers'
